abfe0e45c377479cbacac5d65713af75a68f3a62
Fernando Flores <luiisflores34@gmail.com>
2026-09-26 14:49:32 -0600

    fix: agregar target closest para permitir click en los svg dentro de los botones de accion

--- js/script.js
+++ js/script.js
@@ -11,16 +11,29 @@
 const seccionListaDePeliculas = document.querySelector('.lista-de-peliculas');
 
 seccionListaDePeliculas.addEventListener('click', (event) => {
-    const element = event.target;
+    const btnVisto = event.target.closest('.btn-visto');
+    const btnVerDespues = event.target.closest('.btn-ver-despues')
 
-    if (element.classList.contains('btn-visto')){
-        element.textContent = '¡Visto!';
-        element.classList.add('activo');
-        element.disabled = true;
+    if (btnVisto !== null) {
+        btnVisto.textContent = '¡Visto!';
+        btnVisto.classList.add('activo');
+        btnVisto.disabled = true;
     }
-    else if(element.classList.contains('btn-ver-despues')){
-        element.textContent = '¡Agregado para despues!';
-        element.classList.add('activo');
-        element.disabled = true;
+    else if(btnVerDespues !== null){
+        btnVerDespues.textContent = '¡Agregado para después!';
+        btnVerDespues.classList.add('activo');
+        btnVerDespues.disabled = true;
     }
+
+
+    // if (element.classList.contains('btn-visto')){
+    //     element.textContent = '¡Visto!';
+    //     element.classList.add('activo');
+    //     element.disabled = true;
+    // }
+    // else if(element.classList.contains('btn-ver-despues')){
+    //     element.textContent = '¡Agregado para despues!';
+    //     element.classList.add('activo');
+    //     element.disabled = true;
+    // }
 });
